7d16d60595d358471ae6da93f343b278f0071bdc
Jorge Martin <jorgemctin@gmail.com>
2023-07-18 18:46:24 +0200

    Feat: factory 10 fake() User

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -42,4 +44,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    //Hacer relaciones:
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
+            'email' => fake()->unique()->
+            safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            //añadir role por defecto
+            'role_id' => 2,
             'remember_token' => Str::random(10),
         ];
     }
