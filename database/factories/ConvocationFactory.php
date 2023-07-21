4e38eacb518b726eae398188c32541077182ed6c
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 01:53:46 +0200

    Feat: ConvocationFactory

---
+++ database/factories/ConvocationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Program;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Convocation>
+ */
+class ConvocationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'program_id' => Program::factory(),
+            'description'=>fake()->paragraph(),
+            'status'=>false
+        ];
+    }
+}
