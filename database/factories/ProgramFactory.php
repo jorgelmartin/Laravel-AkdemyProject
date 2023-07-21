09fa015103eb344601218ad29e8c741ccfd83611
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 01:53:27 +0200

    Feat: ProgramFactory

---
+++ database/factories/ProgramFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Program>
+ */
+class ProgramFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            // 'name' => fake()->word(),
+            'name' => fake()->randomElement($array = array ('ciberseguridad','python','php', 'javascript')),
+            'description'=>fake()->paragraph(),
+            'price' => fake()->randomDigit(),
+            // 'pdf' => fake()->randomElement($array = array ('ciberseguridad','python','php', 'javascript')),
+        ];
+    }
+}
