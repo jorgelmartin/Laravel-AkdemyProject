ff0da0a62a35b7eaec740c75f80f4c5e49b950c9
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 01:56:27 +0200

    Refactor: Program Model

---
+++ app/Models/Program.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name', 
+        'description', 
+        'price'
+    ];
+    public function convocation() {
+        return $this->belongsTo(Convocation::class);
+    }
+}
