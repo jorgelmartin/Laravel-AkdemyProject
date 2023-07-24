bbf49b99034315359735de490ba815294c89f72e
Jorge Martin <jorgemctin@gmail.com>
2023-07-24 22:25:49 +0200

    Feat: ProgramController to get al programs

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -10,7 +10,7 @@ class Program extends Model
     use HasFactory;
     protected $fillable = [
         'name', 
-        'description', 
+        'beginning', 
         'price'
     ];
     public function convocation() {
