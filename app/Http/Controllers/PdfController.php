76322e79a99cc72d8c21c956c8c506f659879da2
Jorge Martin <jorgemctin@gmail.com>
2023-07-31 01:48:29 +0200

    Test again

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -1,21 +1,21 @@
 <!-- <?php
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
 use PDF;
 use Illuminate\Http\Request;
 
-class PdfController extends Controller
+// class PdfController extends Controller
 {
-    public function generarPdf()
+    // public function generarPdf()
     {
         $data = [
             'titulo' => 'Ejemplo de PDF con Laravel Dompdf',
             'contenido' => 'Este es un contenido de ejemplo que se mostrará en el PDF.',
         ];
 
-        $pdf = PDF::loadView('pdf.python', $data);
+        // $pdf = PDF::loadView('pdf.python', $data);
 
-        return $pdf->download('Curso_de_Python.pdf');
+        // return $pdf->download('Curso_de_Python.pdf');
     }
-} >
+};
