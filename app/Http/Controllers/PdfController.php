8374fcf67694bf7a7120a2ddbf436437e566dd77
Jorge Martin <jorgemctin@gmail.com>
2023-08-04 08:53:38 +0200

    test again

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -1,21 +1,22 @@
-<!-- <?php
+<!-- <   ?  -->
+<!-- php  -->
 
-// namespace App\Http\Controllers;
+<!-- // namespace App\Http\Controllers;
 
-use PDF;
-use Illuminate\Http\Request;
+// use PDF;
+// use Illuminate\Http\Request;
 
 // class PdfController extends Controller
-{
+// {
     // public function generarPdf()
-    {
-        $data = [
-            'titulo' => 'Ejemplo de PDF con Laravel Dompdf',
-            'contenido' => 'Este es un contenido de ejemplo que se mostrará en el PDF.',
-        ];
+    // {
+        // $data = [
+        //     'titulo' => 'Ejemplo de PDF con Laravel Dompdf',
+        //     'contenido' => 'Este es un contenido de ejemplo que se mostrará en el PDF.',
+        // ];
 
         // $pdf = PDF::loadView('pdf.python', $data);
 
         // return $pdf->download('Curso_de_Python.pdf');
-    }
-};
+//     }
+// }; -->
