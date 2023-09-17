3e0556d621a8e6b527e620df120d12e5e9b24eb0
Jorge Martin <jorgemctin@gmail.com>
2023-09-17 20:37:36 +0200

    Fix: naming inscriptions at migrations

--- database/migrations/2023_07_20_214205_create_inscriptions_table.php
+++ database/migrations/2023_07_20_214205_create_inscriptions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('inscription', function (Blueprint $table) {
+        Schema::create('inscriptions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('convocation_id');
             $table->unsignedBigInteger('user_id');
@@ -37,6 +37,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('inscription');
+        Schema::dropIfExists('inscriptions');
     }
 };
